75971dad815e2917ab4af17c14c74ddbb39f4d6b
Matthieumoto <136125610+Matthieumoto@users.noreply.github.com>
2023-08-31 14:14:42 +0200

    4

--- script.js
+++ script.js
@@ -1,37 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
     const gallery = document.querySelector('.image-gallery');
     const boxes = document.querySelectorAll('.gallery-box');
-    const leftArrow = document.querySelector('.arrow.left');
-    const rightArrow = document.querySelector('.arrow.right');
     let currentIndex = 0;
 
-    updateArrows(); // Appel initial pour désactiver les flèches si nécessaire
+    boxes.forEach((box, index) => {
+        box.addEventListener('mouseover', () => {
+            box.style.transform = 'scale(1.2)'; // Zoom sur l'article au passage de la souris
+            box.style.opacity = 1; // Rétablit l'opacité totale au passage de la souris
+        });
 
-    leftArrow.addEventListener('click', () => {
-        currentIndex = (currentIndex - 1 + boxes.length) % boxes.length;
-        updateGalleryPosition();
-        updateArrows(); // Mise à jour des flèches après le changement d'image
+        box.addEventListener('mouseout', () => {
+            box.style.transform = 'scale(1)'; // Rétablir la taille normale lorsque la souris quitte l'article
+            box.style.opacity = 0.5; // Appliquer l'opacité réduite après la sortie de la souris
+        });
     });
 
-    rightArrow.addEventListener('click', () => {
-        currentIndex = (currentIndex + 1) % boxes.length;
-        updateGalleryPosition();
-        updateArrows(); // Mise à jour des flèches après le changement d'image
-    });
-
-    function updateArrows() {
-        if (currentIndex === 0) {
-            leftArrow.style.pointerEvents = 'none'; // Désactiver la flèche gauche à la première image
-        } else {
-            leftArrow.style.pointerEvents = 'auto'; // Activer la flèche gauche pour les autres images
-        }
-
-        if (currentIndex === boxes.length - 1) {
-            rightArrow.style.pointerEvents = 'none'; // Désactiver la flèche droite à la dernière image
-        } else {
-            rightArrow.style.pointerEvents = 'auto'; // Activer la flèche droite pour les autres images
-        }
-    }
 
     function updateGalleryPosition() {
         const translateX = -currentIndex * 100;
@@ -39,11 +22,8 @@ document.addEventListener("DOMContentLoaded", function() {
             const offset = index - currentIndex;
             const xOffset = offset * 110; // Ajustez la valeur d'espacement ici
             box.style.transform = `translateX(calc(${translateX}% + ${xOffset}px))`;
-            if (index !== currentIndex) {
-                box.style.opacity = 0.5; // Réduit l'opacité pour les images non sélectionnées
-            } else {
-                box.style.opacity = 1; // Rétablit l'opacité pour l'image sélectionnée
-            }
+            box.style.opacity = 0.5; // Appliquer l'opacité réduite pour toutes les images
         });
+        boxes[currentIndex].style.opacity = 1; // Rétablit l'opacité totale pour l'image sélectionnée
     }
 });
